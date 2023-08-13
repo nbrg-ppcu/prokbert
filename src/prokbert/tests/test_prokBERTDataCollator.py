d6381f3b60c72c52e64323a6cdc937c0e5915161
Ligeti Balázs <ligeti.balazs@itk.ppke.hu>
2023-08-13 22:23:08 +0200

    Testing ...

--- src/prokbert/tests/test_prokBERTDataCollator.py
+++ src/prokbert/tests/test_prokBERTDataCollator.py
@@ -2,16 +2,23 @@ import unittest
 import torch
 from transformers import BertTokenizer
 
+from prokbert_tokenizer import ProkBERTTokenizer
+from ProkBERTDataCollator import ProkBERTDataCollator
+
+
+
+
 class TestProkBERTDataCollator(unittest.TestCase):
 
     def setUp(self):
         # Setup code: this runs before every individual test
-        self.tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+        self.tokenizer = ProkBERTTokenizer(tokenization_params={'shift': 2}, operation_space='sequence')
         self.collator = ProkBERTDataCollator(tokenizer=self.tokenizer, mlm_probability=0.15)
 
     def test_masking(self):
-        # A simple test to check if the input tensor is masked properly
-        inputs = torch.tensor([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+        segment = 'AATCAAGGAATTATTATCGTT'
+        input_ids = self.tokenizer.batch_encode_plus([segment])["input_ids"][0]
+        inputs = torch.tensor([input_ids])
         masked_inputs, labels = self.collator.torch_mask_tokens(inputs)
 
         # Check if shape remains the same
@@ -35,7 +42,22 @@ class TestProkBERTDataCollator(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.collator.set_mask_neighborhood_params(1, -1)
 
-    # ... Add more tests ...
+    def test_tokenization_example(self):
+        segment = 'AATCAAGGAATTATTATCGTT'
+        tokens, kmers = self.tokenizer.tokenize(segment, all=True)
+        # Validate tokenization
+        self.assertIn('AATCAA', kmers[0])
+        self.assertIn('TTATTA', kmers[0])
+
+        restored = ''.join(self.tokenizer.batch_decode([tokens[0]]))
+        self.assertEqual(segment, restored)
+
+    def test_unknown_token_handling(self):
+        segment = 'AASTTAAGGAATTATTATCGT'
+        tokens, kmers = self.tokenizer.tokenize(segment, all=True)
+        # Validate tokenization contains unknown token
+        self.assertIn('NNNNNN', kmers[0])
+
 
 
 if __name__ == '__main__':
