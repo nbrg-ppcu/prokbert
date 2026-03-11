Adding loading fix to the other classes; normalizing loading

src/prokbert/models.py
@@ -436,6 +436,62 @@ class MegatronBertConfig(PretrainedConfig):
 
 
 class MegatronBertEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.word_embeddings = nn.Embedding(
+            config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id
+        )
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
+
+    @staticmethod
+    def _make_position_ids(seq_length: int, device: torch.device, past_key_values_length: int = 0):
+        return torch.arange(
+            past_key_values_length,
+            past_key_values_length + seq_length,
+            dtype=torch.long,
+            device=device,
+        ).unsqueeze(0)
+
+    def forward(
+        self,
+        input_ids=None,
+        token_type_ids=None,
+        position_ids=None,
+        inputs_embeds=None,
+        past_key_values_length: int = 0,
+    ):
+        if input_ids is not None:
+            input_shape = input_ids.size()
+            device = input_ids.device
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+            device = inputs_embeds.device
+
+        seq_length = input_shape[1]
+
+        if position_ids is None and self.position_embedding_type == "absolute":
+            position_ids = self._make_position_ids(
+                seq_length, device, past_key_values_length
+            )
+
+        if token_type_ids is None:
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+
+        if inputs_embeds is None:
+            inputs_embeds = self.word_embeddings(input_ids)
+
+        embeddings = inputs_embeds + self.token_type_embeddings(token_type_ids)
+
+        if self.position_embedding_type == "absolute":
+            embeddings = embeddings + self.position_embeddings(position_ids)
+
+        return self.dropout(embeddings)
+    
+
+class DeprecatedMegatronBertEmbeddings(nn.Module):
     """Construct the embeddings from word, position and token_type embeddings."""
 
     def __init__(self, config):
@@ -993,33 +1049,17 @@ class MegatronBertPreTrainedModel(PreTrainedModel):
             module.bias.data.zero_()
 
 #@auto_docstring
+
 class MegatronBertModel(MegatronBertPreTrainedModel):
-    """
-
-    The model can behave as an encoder (with only self-attention) as well as a decoder, in which case a layer of
-    cross-attention is added between the self-attention layers, following the architecture described in [Attention is
-    all you need](https://huggingface.co/papers/1706.03762) by Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit,
-    Llion Jones, Aidan N. Gomez, Lukasz Kaiser and Illia Polosukhin.
-
-    To behave as an decoder the model needs to be initialized with the `is_decoder` argument of the configuration set
-    to `True`. To be used in a Seq2Seq model, the model needs to initialized with both `is_decoder` argument and
-    `add_cross_attention` set to `True`; an `encoder_hidden_states` is then expected as an input to the forward pass.
-    """
+    _no_split_modules = ["MegatronBertEmbeddings", "MegatronBertLayer"]
 
     def __init__(self, config, add_pooling_layer=False):
-        r"""
-        add_pooling_layer (bool, *optional*, defaults to `True`):
-            Whether to add a pooling layer
-        """
         super().__init__(config)
         self.config = config
-
+        self.gradient_checkpointing = False
         self.embeddings = MegatronBertEmbeddings(config)
         self.encoder = MegatronBertEncoder(config)
-
         self.pooler = MegatronBertPooler(config) if add_pooling_layer else None
-
-        # Initialize weights and apply final processing
         self.post_init()
 
     def get_input_embeddings(self):
@@ -1322,179 +1362,6 @@ class ProkBertConfig(MegatronBertConfig):
         if self.classifier_head_type not in {"linear", "mlp", "curricular"}:
             raise ValueError(f"Unsupported classifier_head_type={self.classifier_head_type}")
         
-
-class DepredactedProkBertConfig(MegatronBertConfig):
-    model_type = "prokbert"
-
-    def __init__(
-        self,
-        kmer: int = 6,
-        shift: int = 1,
-        num_class_labels: int = 2,
-        classification_dropout_rate: float = 0.1,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.kmer = kmer
-        self.shift = shift
-        self.num_class_labels = num_class_labels
-        self.classification_dropout_rate = classification_dropout_rate
-
-class DeprecatedProkBertConfigCurr(ProkBertConfig):
-    model_type = "prokbert"
-
-    def __init__(
-        self,
-        bert_base_model = "neuralbioinfo/prokbert-mini",
-        curricular_face_m = 0.5,
-        curricular_face_s=64.,
-        curricular_num_labels = 2,
-        curriculum_hidden_size = -1,
-        classification_dropout_rate = 0.0,
-        **kwargs,
-    ):
-        super().__init__( **kwargs)
-        self.curricular_num_labels = curricular_num_labels
-        self.curricular_face_m = curricular_face_m
-        self.curricular_face_s = curricular_face_s
-        self.bert_base_model = bert_base_model
-        self.curriculum_hidden_size = curriculum_hidden_size
-        self.classification_dropout_rate = classification_dropout_rate
-
-class DeprecatedProkBertClassificationConfig(ProkBertConfig):
-    model_type = "prokbert"
-    def __init__(
-        self,
-        num_labels: int = 2,
-        classification_dropout_rate: float = 0.1,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.num_labels = num_labels
-        self.classification_dropout_rate = classification_dropout_rate
-
-
-
-class BertForBinaryClassificationWithPooling(nn.Module):
-    """
-    ProkBERT model for binary classification with custom pooling.
-
-    This model extends a pre-trained `MegatronBertModel` by adding a weighting layer
-    to compute a weighted sum over the sequence outputs, followed by a classifier.
-
-    Attributes:
-        base_model (MegatronBertModel): The base BERT model.
-        weighting_layer (nn.Linear): Linear layer to compute weights for each token.
-        dropout (nn.Dropout): Dropout layer.
-        classifier (nn.Linear): Linear layer for classification.
-    """
-    def __init__(self, base_model: MegatronBertModel):
-        """
-        Initialize the BertForBinaryClassificationWithPooling model.
-
-        Args:
-            base_model (MegatronBertModel): A pre-trained `MegatronBertModel` instance.
-        """
-
-        super(BertForBinaryClassificationWithPooling, self).__init__()
-        self.base_model = base_model
-        self.base_model_config_dict = base_model.config.to_dict()
-        self.hidden_size = self.base_model_config_dict['hidden_size']
-        self.dropout_rate = self.base_model_config_dict['hidden_dropout_prob']
-
-        self.weighting_layer = nn.Linear(self.hidden_size, 1)
-        self.dropout = nn.Dropout(self.dropout_rate)
-        self.classifier = nn.Linear(self.hidden_size, 2)
-
-    def forward(self, input_ids, attention_mask=None, labels=None, output_hidden_states=False, output_pooled_output=False):
-        # Modified call to base model to include output_hidden_states
-        outputs = self.base_model(input_ids, attention_mask=attention_mask, output_hidden_states=output_hidden_states)
-        sequence_output = outputs[0]
-
-        # Compute weights for each position in the sequence
-        weights = self.weighting_layer(sequence_output)
-        weights = torch.nn.functional.softmax(weights, dim=1)
-
-        # Compute weighted sum
-        pooled_output = torch.sum(weights * sequence_output, dim=1)
-
-        # Classification head
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
-
-        # Prepare the output as a dictionary
-        output = {"logits": logits}
-
-        # Include hidden states in output if requested
-        if output_hidden_states:
-            output["hidden_states"] = outputs.hidden_states
-        if output_pooled_output:
-            output["pooled_output"] = pooled_output
-
-        # If labels are provided, compute the loss
-        if labels is not None:
-            loss_fct = torch.nn.CrossEntropyLoss()
-            loss = loss_fct(logits.view(-1, 2), labels.view(-1))
-            output["loss"] = loss
-
-        return output
-
-    def save_pretrained(self, save_directory):
-        """
-        Save the model weights and configuration in a directory.
-
-        Args:
-            save_directory (str): Directory where the model and configuration can be saved.
-        """
-        print('The save pretrained is called!')
-        if not os.path.exists(save_directory):
-            os.makedirs(save_directory)
-
-        model_path = os.path.join(save_directory, "pytorch_model.bin")
-        torch.save(self.state_dict(), model_path)
-        print(f'The save directory is: {save_directory}')
-        self.base_model.config.save_pretrained(save_directory)
-
-    @classmethod
-    def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
-        """
-        Load the model weights and configuration from a local directory or Hugging Face Hub.
-
-        Args:
-            pretrained_model_name_or_path (str): Directory path where the model and configuration were saved, or name of the model in Hugging Face Hub.
-
-        Returns:
-            model: Instance of BertForBinaryClassificationWithPooling.
-        """
-        # Determine if the path is local or from Hugging Face Hub
-        if os.path.exists(pretrained_model_name_or_path):
-            # Path is local
-            if 'config' in kwargs:
-                print('Config is in the parameters')
-                config = kwargs['config']
-
-            else:
-                config = MegatronBertConfig.from_pretrained(pretrained_model_name_or_path, **kwargs)
-            base_model = MegatronBertModel(config=config)
-            model = cls(base_model=base_model)
-            model_path = os.path.join(pretrained_model_name_or_path, "pytorch_model.bin")
-            model.load_state_dict(torch.load(model_path, 
-                                             map_location=torch.device('cpu'),
-                                            weights_only=True))
-        else:
-            # Path is from Hugging Face Hub
-            config = kwargs.pop('config', None)
-            if config is None:
-                config = MegatronBertConfig.from_pretrained(pretrained_model_name_or_path, **kwargs)
-
-            base_model = MegatronBertModel(config=config)
-            model = cls(base_model=base_model)
-            model_file = cached_file(pretrained_model_name_or_path, "pytorch_model.bin")
-            model.load_state_dict(torch.load(model_file, map_location=torch.device('cpu'), weights_only=True))
-
-        return model
-
-
 class ProkBertPreTrainedModel(MegatronBertPreTrainedModel):
     """
     An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
@@ -1744,8 +1611,6 @@ class ProkBertForCurricularClassification(_SafeFromPretrainedMixin, ProkBertPreT
                                                 self.config.curricular_num_labels,
                                                 m=self.config.curricular_face_m,
                                                 s=self.config.curricular_face_s)
-
-
         self.loss_fct = torch.nn.CrossEntropyLoss()
         self.post_init()
 