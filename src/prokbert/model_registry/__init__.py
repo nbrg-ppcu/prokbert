c6df8b553180f40c239880d389dd1a17bd32e613
Ligeti Balázs <obalasz@gmail.com>
2026-03-25 11:43:36 +0100

    Adding the modelling features

--- src/prokbert/model_registry/__init__.py
+++ src/prokbert/model_registry/__init__.py
@@ -1,3 +1,3 @@
-from .training_helper import TrainingHelper, get_tokenize_function
+from .training_helper import TrainingHelper
 
-__all__ = ["TrainingHelper", "get_tokenize_function"]
+__all__ = ["TrainingHelper"]
